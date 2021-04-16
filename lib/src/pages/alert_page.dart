0110b0c94728ef0bb4c0eae7d638d9415156f386
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-04-16 14:51:42 -0500

    Usando Alerts

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Alert Page'),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.arrow_left),
-      onPressed: (){
-        Navigator.pop(context);
-      },
-    ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Display Alert'),
+          onPressed: () => _showAlert(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.yellow[800],
+            shape: StadiumBorder(),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_left),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
     );
+  }
+
+  _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Títulos'),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Alertado'),
+                FlutterLogo(
+                  size: 100,
+                ),
+              ],
+            ),
+            actions: [
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('CANCEL'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
+  final imageUrl1 =
+      'https://cdn.vox-cdn.com/thumbor/Jy3ezPuozPuRA1uTBwUxA-yz7TQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22176756/TronLegacy2793.jpg';
 
+  final imageUrl2 =
+      'https://s3.us-east-1.amazonaws.com/dexerto-assets-production-cbbdf288/uploads/2021/02/10142012/Tron-Legacy.jpg?w=620';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
-
-    ),
-     floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.arrow_left),
-      onPressed: (){
-        Navigator.pop(context);
-      },
-    )
+        actions: <Widget>[
+          Container(
+            child: CircleAvatar(
+              child: Text('CA'),
+              backgroundColor: Colors.black87,
+            ),
+            margin: EdgeInsets.only(right: 20.0),
+          ),
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(imageUrl1),
+              radius: 25,
+            ),
+            margin: EdgeInsets.only(right: 20.0),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: FadeInImage(
+            image: NetworkImage(imageUrl2),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          ),
+        ),
+      ),
     );
   }
 }
