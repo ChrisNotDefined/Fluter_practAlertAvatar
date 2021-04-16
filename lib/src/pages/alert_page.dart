111761d70a41bf290d074a6c50825b300baa5d77
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-04-16 16:04:36 -0500

    Practica de Avatar y Alerts

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,13 +8,30 @@ class AlertPage extends StatelessWidget {
         title: Text('Alert Page'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Display Alert'),
-          onPressed: () => _showAlert(context),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.yellow[800],
-            shape: StadiumBorder(),
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: Text('Display Alert'),
+              onPressed: () => _showAlert(context),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.yellow[800],
+                shape: StadiumBorder(),
+              ),
+            ),
+            ElevatedButton.icon(
+              label: Text('Display My Alert'),
+              icon: Icon(Icons.alarm),
+              onPressed: () => _showAlertRouted(
+                context,
+                'avatar',
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green[800],
+                shape: StadiumBorder(),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -28,37 +45,75 @@ class AlertPage extends StatelessWidget {
 
   _showAlert(BuildContext context) {
     showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Títulos'),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Alertado'),
-                FlutterLogo(
-                  size: 100,
-                ),
-              ],
-            ),
-            actions: [
-              FlatButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              FlatButton(
-                child: Text('CANCEL'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Títulos'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Alertado'),
+              FlutterLogo(
+                size: 100,
               ),
             ],
-          );
-        });
+          ),
+          actions: [
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _showAlertRouted(BuildContext context, String routeName) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Redireccionar?'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Vas a abandonar tu página actual, ¿deseas continuar?'),
+              Image(
+                image: AssetImage('assets/alerta.png'),
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.popAndPushNamed(context, routeName);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 class AvatarPage extends StatelessWidget {
   final imageUrl1 =
       'https://cdn.vox-cdn.com/thumbor/Jy3ezPuozPuRA1uTBwUxA-yz7TQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22176756/TronLegacy2793.jpg';
-
   final imageUrl2 =
       'https://s3.us-east-1.amazonaws.com/dexerto-assets-production-cbbdf288/uploads/2021/02/10142012/Tron-Legacy.jpg?w=620';
+  final imageUrl3 =
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/capitan-america-wyatt-russell-falcon-y-el-soldado-de-invierno-marvel-1616347629.jpg?resize=480:*";
+  final imageUrl4 =
+      "https://d25nlln9isiu5y.cloudfront.net/wp-content/uploads/2021/02/05141335/SWWVePF1-1024x576.jpg";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +17,7 @@ class AvatarPage extends StatelessWidget {
         actions: <Widget>[
           Container(
             child: CircleAvatar(
-              child: Text('CA'),
+              child: Text('DNA'),
               backgroundColor: Colors.black87,
             ),
             margin: EdgeInsets.only(right: 20.0),
@@ -23,6 +26,46 @@ class AvatarPage extends StatelessWidget {
             child: CircleAvatar(
               backgroundImage: NetworkImage(imageUrl1),
               radius: 25,
+            ),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.yellow[500],
+                  width: 4,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 20,
+                      offset: Offset(0, 5)),
+                ]),
+            margin: EdgeInsets.only(right: 20.0),
+          ),
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(imageUrl3),
+              radius: 25,
+            ),
+            decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                border: new Border.all(color: Colors.red, width: 4.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
+            margin: EdgeInsets.only(right: 20.0),
+          ),
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(imageUrl4),
+              radius: 25,
+            ),
+            decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              border: new Border.all(color: Colors.pink, width: 4.0),
             ),
             margin: EdgeInsets.only(right: 20.0),
           ),
